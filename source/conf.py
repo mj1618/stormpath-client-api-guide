edf6591d6a2be0d8bea07bbb410b59746d138992
Randall Degges <r@rdegges.com>
2016-11-30 14:00:37 -0600

    Using the Stormpath Sphinx theme.

--- source/conf.py
+++ source/conf.py
@@ -20,8 +20,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import sphinx_rtd_theme
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -126,9 +124,9 @@ todo_include_todos = False
 #
 
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "stormpath"
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = ['_themes']
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
